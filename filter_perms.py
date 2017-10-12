01fc71fb5884ff92f06ad41bc684628a780f36c2
Rick Toews <rick.toews@gmail.com>
2017-10-11 23:06:27 -0700

    Debugging and adjusting of pattern checking.

--- filter_perms.py
+++ filter_perms.py
@@ -13,46 +13,37 @@ def check_exact(exact, perm, pattern):
             pattern[i] = '_'
             count += 1
     if count == exact:
-        return ''.join(pattern)
+        return True
     else:
         return False    
         
 
 def check_inexact(inexact, perm, pattern):
     perm = list(perm)
+    orig_pattern = pattern
     pattern = list(pattern)
     pattern_len = len(pattern)
+    for i in range(pattern_len):
+        if pattern[i] == perm[i]:
+            perm[i] = '_'
+            pattern[i] = '_'
     count = 0
     for i in range(pattern_len):
         if pattern[i] != '_':
             if pattern[i] != perm[i] and pattern[i] in perm:
                 count += 1
     if count == inexact:
-        return ''.join(pattern)
+        return True
     else:
         return False
 
 
-def final_check(perm, pattern):
-    result = True
-    perm = list(perm)
-    pattern = list(pattern)
-    pattern_len = len(pattern)
-    for i in range(pattern_len):
-        if pattern[i] != '_':
-            if pattern[i] in perm:
-                result = False
-    return result
-
-
 def is_match(exact, inexact, perm, pattern):
-    pattern = check_exact(exact, perm, pattern)
-    if bool(pattern):
-        pattern = check_innexact(inexact, perm, pattern)
-    if bool(pattern):
-        pattern = final_check(perm, pattern)
+    exact_match = check_exact(exact, perm, pattern)
+    if exact_match:
+        inexact_match = check_inexact(inexact, perm, pattern)
 
-    return bool(pattern)
+    return exact_match and inexact_match
 
 
 def filter_perms(exact, inexact, pattern):
