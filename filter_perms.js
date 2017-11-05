0d1122175f08bcb9b410f5e6584e774aa07010c0
Rick Toews <rick.toews@gmail.com>
2017-11-04 18:01:40 -0700

    Make js versions.

---
+++ filter_perms.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var permutations = require('./perms.js');
+console.log('permutations', permutations);
+var perms = permutations.build(6, 4);
+
+function check_exact(exact, perm, pattern) {
+    var perm = perm.split('');
+    var pattern = pattern.split('');
+    var pattern_len = pattern.length;
+    var count = 0;
+    for (let i = 0; i < pattern_len; i++) {
+        if (pattern[i] === perm[i]) {
+            pattern[i] = '_';
+            count += 1;
+	}
+    }
+    if (count == exact) {
+        return true;
+    } else {
+        return false;
+    }
+}
+        
+
+function check_inexact(inexact, perm, pattern) {
+    var perm = perm.split('');
+    var orig_pattern = pattern;
+    var pattern = pattern.split('');
+    var pattern_len = pattern.length;
+    for (let i = 0; i < pattern_len; i++) {
+        if (pattern[i] === perm[i]) {
+            perm[i] = '_';
+            pattern[i] = '_';
+        }
+    }
+    var count = 0;
+    for (let i = 0; i < patter_len; i++) {
+        if (pattern[i] !== '_') {
+            if (pattern[i] !== perm[i] && perm.indexOf(pattern[i]) !== -1) {
+                let ndx = perm.index(pattern[i]);
+                perm[ndx] = '_';
+                count++;
+            }
+        }
+    }
+
+    if (count === inexact) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function is_match(exact, inexact, perm, pattern) {
+    var exact_match = check_exact(exact, perm, pattern);
+    if (exact_match) {
+        inexact_match = check_inexact(inexact, perm, pattern);
+    }
+
+    return exact_match && inexact_match;
+}
+
+function filter_perms(exact, inexact, pattern) {
+    var filtered = [];
+    perms.forEach((p) => {
+        if (is_match(exact, inexact, p, pattern)) {
+            filtered.push(p);
+        }
+    });
+
+    return filtered;
+}
